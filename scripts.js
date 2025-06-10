034ff9ff4e5d91ab72464ce7ce69f168c09bef33
bangvv <vubang060695@gmail.com>
2025-06-11 01:08:50 +0700

    update

--- scripts.js
+++ scripts.js
@@ -228,10 +228,17 @@ audioElem.addEventListener("loadedmetadata", () => {
 
 // Format giây sang mm:ss
 function formatTime(seconds) {
-  const mins = Math.floor(seconds / 60);
+  const hrs = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
   const secs = Math.floor(seconds % 60);
-  return `${mins}:${secs.toString().padStart(2, "0")}`;
+
+  if (hrs > 0) {
+    return `${hrs}:${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+  } else {
+    return `${mins}:${secs.toString().padStart(2, "0")}`;
+  }
 }
+
 
 // Cập nhật progress thanh audio
 function updateProgress() {
